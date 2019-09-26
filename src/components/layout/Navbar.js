3e2dac9a2d3f84c674575242f8ea37afc3f65734
Sam Jackson <samuelsjackson7@gmail.com>
2019-09-26 14:41:18 -0400

    added UserItem component, hardcoded 1 user's info

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
+class Navbar extends Component {
   // if no props are passed in App.js, these will be used.
   // any props passed in App.js with these names will overwrite those defaultProps
   static defaultProps = {
@@ -16,11 +16,11 @@ export class Navbar extends Component {
 
   render() {
     return (
-      <div>
+      <nav className='navbar bg-primary'>
         <h1>
           <i className={this.props.icon} /> {this.props.title}
         </h1>
-      </div>
+      </nav>
     );
   }
 }
