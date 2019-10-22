d4c2c8c79f2aa3b16b0c79df1920bb520c97e1ae
Sam Jackson <samuelsjackson7@gmail.com>
2019-10-22 16:10:14 -0400

    Create Reducer & Actions: moved searchUsers from App.js to GithubState.js.

--- src/App.js
+++ src/App.js
@@ -29,18 +29,6 @@ const App = () => {
   //   this.setState({ users: res.data, loading: false });
   // }
 
-  // Search Github Users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   // Get single GitHub user
   const getUser = async username => {
     setLoading(true);
@@ -91,7 +79,6 @@ const App = () => {
                 render={props => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
