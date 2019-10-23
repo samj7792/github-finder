e54840729c37ea5bf80a03e45d1c8cf8e2e768f3
Sam Jackson <samuelsjackson7@gmail.com>
2019-10-23 14:51:45 -0400

    moved Repos state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -49,6 +49,18 @@ const GithubState = props => {
   };
 
   // Get Repos
+  const getUserRepos = async username => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  };
 
   // Clear Users
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -65,7 +77,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}
